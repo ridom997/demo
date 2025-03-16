86a9e15844c210f8d3908ad7cc259e7078da9b2b
Pedro Rios Dominguez <pedrorido97@gmail.com>
2025-03-16 16:40:34 -0500

    first commit

---
+++ src/main/java/com/ridomz/app/demo/MyBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.ridomz.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusinessApplication.class, args);
+	}
+
+}
